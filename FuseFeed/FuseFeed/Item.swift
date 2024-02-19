c95a98d5649cb736ef0bec55608f96724af1f0b4
Tyler Morgan <tylermorgan210@gmail.com>
2024-02-19 14:44:30 -0500

    Adding intial application

---
+++ FuseFeed/FuseFeed/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FuseFeed
+//
+//  Created by Morgan, Tyler on 2/19/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FuseFeed/FuseFeed/Models/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  FuseFeed
+//
+//  Created by Morgan, Tyler on 2/19/24.
+//
+
+import Foundation
+
+/**
+ Feed items for a specific feed.
+ */
+struct FeedItem {
+    //let platformLogo: Image
+    let title: String
+    let message: String
+}
